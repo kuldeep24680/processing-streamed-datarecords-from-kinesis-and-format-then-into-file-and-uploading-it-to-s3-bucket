ccd6844f9da1ca4a2be2b333afc10f01198ca90f
Kuldeep Chouhan <30400214+kuldeep24680@users.noreply.github.com>
2018-11-06 14:41:29 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,66 +6,46 @@ import boto3
 import ast
 def lambda_handler(event, context):
     for record in event['Records']:
-       #Kinesis data is base64 encoded so decode here
+       #Kinesis data is base64 encoded so data is decoded here
        s3 = boto3.client('s3')
        payload=base64.b64decode(record["kinesis"]["data"])
        print("Decoded payload: " + str(payload))
       
-       metadata_check1 = payload.decode("utf-8")
-       metadata_check = ast.literal_eval(metadata_check1)
+       decoded_data = payload.decode("utf-8")
+       datarecord = ast.literal_eval(decoded_data)
        
-       print(metadata_check)
+       print(datarecord)
        
-       if not metadata_check.get('metadata',{}).get('source',{}) or not metadata_check.get('metadata',{}).get('retention',{})  or not metadata_check.get('metadata',{}).get('business',{}) or not metadata_check.get('metadata',{}).get('usecase',{}) or not metadata_check.get('metadata',{}).get('ingestion_owner',{}) or not metadata_check.get('metadata',{}).get('level1_ownership',{}) or not metadata_check.get('metadata',{}).get('level2_ownership',{}) or not metadata_check.get('metadata',{}).get('confidentiality',{})  or not metadata_check.get('metadata',{}).get('quality',{})  or not metadata_check.get('metadata',{}).get('refresh_velocity',{}):
+       if not datarecord.get('metadata',{}).get('source',{}) or not datarecord.get('metadata',{}).get('retention',{})  or not datarecord.get('metadata',{}).get('business',{}) or not datarecord.get('metadata',{}).get('usecase',{}) or not datarecord.get('metadata',{}).get('ingestion_owner',{}) or not datarecord.get('metadata',{}).get('level1_ownership',{}) or not datarecord.get('metadata',{}).get('level2_ownership',{}) or not datarecord.get('metadata',{}).get('confidentiality',{})  or not datarecord.get('metadata',{}).get('quality',{})  or not datarecord.get('metadata',{}).get('refresh_velocity',{}):
            
            data=byte(payload.decode("utf-8"))
-           url= metadata_check["metadata"]["url"]
+           url= datarecord["metadata"]["url"]
            prefix=""
            url=url.split('/')
            n=len(url)
            for i in range(1,n-1):
             prefix=prefix+url[i]+'/'
            prefix=prefix+url[n-1]
-           s3.Bucket('onepurchase-test-s3-quarantinebucket').put_object(Key=prefix, Body=data, ServerSideEncryption="AES256")
-           print("Uploaded to Quarantine Bucket due to missing metadata tags")
+           s3.Bucket('<s3-backup-bucket>').put_object(Key=prefix, Body=data, ServerSideEncryption="AES256")
+           print("Uploaded to Backup Bucket due to missing metadata tags")
     
            
            
-       elif metadata_check["metadata"]["source"]=='' or metadata_check["metadata"]["retention"]=='' or metadata_check["metadata"]["business"]==''  or metadata_check["metadata"]["usecase"]==''  or metadata_check["metadata"]["ingestion_owner"]==''  or metadata_check["metadata"]["expiration"]==''  or metadata_check["metadata"]["level1_ownership"]==''  or metadata_check["metadata"]["level2_ownership"]==''  or metadata_check["metadata"]["confidentiality"]==''  or metadata_check["metadata"]["quality"]=='' or metadata_check["metadata"]["refresh_velocity"]=='':
+       elif datarecord["metadata"]["source"]=='' or datarecord["metadata"]["retention"]=='' or datarecord["metadata"]["business"]==''  or datarecord["metadata"]["usecase"]==''  or datarecord["metadata"]["ingestion_owner"]==''  or datarecord["metadata"]["expiration"]==''  or datarecord["metadata"]["level1_ownership"]==''  or datarecord["metadata"]["level2_ownership"]==''  or datarecord["metadata"]["confidentiality"]==''  or datarecord["metadata"]["quality"]=='' or datarecord["metadata"]["refresh_velocity"]=='':
                        
-            source=metadata_check["metadata"]["source"]
-            retention= metadata_check["metadata"]["retention"]
-            business=metadata_check["metadata"]["business"]
-            usecase=metadata_check["metadata"]["usecase"]
-            ingestion_owner=metadata_check["metadata"]["ingestion_owner"]
-            expiration=metadata_check["metadata"]["expiration"]
-            level1_ownership=metadata_check["metadata"]["level1_ownership"]
-            level2_ownership=metadata_check["metadata"]["level2_ownership"]
-            confidentiality=metadata_check["metadata"]["confidentiality"]
-            quality=metadata_check["metadata"]["quality"]
-            refresh_velocity=metadata_check["metadata"]["refresh_velocity"]
-            
-            print("testing for source: "+ metadata_check["metadata"]["source"])
-            print("testing for prop: "+ str(metadata_check["prop"]))
-            print("testing for timestamp: "+ str(metadata_check["timestamp"]))
-            print("testing for retention: "+ metadata_check["metadata"]["retention"])
-            print("testing for business: "+ metadata_check["metadata"]["business"])
-            print("testing for data: "+ str(metadata_check["data"]))
-            print("testing for usecase: "+ metadata_check["metadata"]["usecase"])
-            print("testing for ingestion owner: "+ metadata_check["metadata"]["ingestion_owner"])
-            print("testing for level1_ownership: "+ metadata_check["metadata"]["level1_ownership"])
-            print("testing for level2_ownership: "+ metadata_check["metadata"]["level2_ownership"])
-            print("testing for confidentiality: "+ metadata_check["metadata"]["confidentiality"])
-            print("testing for expiration: "+ metadata_check["metadata"]["expiration"])
-            print("testing for URL: "+ metadata_check["metadata"]["url"])
-            print("testing for quality: "+ metadata_check["metadata"]["quality"])
-            print("testing for refresh_velocity: "+ metadata_check["metadata"]["refresh_velocity"])
-            
-            
-            
-            
-            data= BytesIO(metadata_check["data"].encode())
-            url= metadata_check["metadata"]["url"]
+            source=datarecord["metadata"]["source"]
+            retention= datarecord["metadata"]["retention"]
+            business=datarecord["metadata"]["business"]
+            usecase=datarecord["metadata"]["usecase"]
+            ingestion_owner=datarecord["metadata"]["ingestion_owner"]
+            expiration=datarecord["metadata"]["expiration"]
+            level1_ownership=datarecord["metadata"]["level1_ownership"]
+            level2_ownership=datarecord["metadata"]["level2_ownership"]
+            confidentiality=datarecord["metadata"]["confidentiality"]
+            quality=datarecord["metadata"]["quality"]
+            refresh_velocity=datarecord["metadata"]["refresh_velocity"] 
+            data= BytesIO(datarecord["data"].encode())
+            url= datarecord["metadata"]["url"]
             prefix=""
             url=url.split('/')
             n=len(url)
@@ -91,39 +71,20 @@ def lambda_handler(event, context):
            
        else:
            
-            source=metadata_check["metadata"]["source"]
-            retention= metadata_check["metadata"]["retention"]
-            business=metadata_check["metadata"]["business"]
-            usecase=metadata_check["metadata"]["usecase"]
-            ingestion_owner=metadata_check["metadata"]["ingestion_owner"]
-            expiration=metadata_check["metadata"]["expiration"]
-            level1_ownership=metadata_check["metadata"]["level1_ownership"]
-            level2_ownership=metadata_check["metadata"]["level2_ownership"]
-            confidentiality=metadata_check["metadata"]["confidentiality"]
-            quality=metadata_check["metadata"]["quality"]
-            refresh_velocity=metadata_check["metadata"]["refresh_velocity"]
+            source=datarecord["metadata"]["source"]
+            retention= datarecord["metadata"]["retention"]
+            business=datarecord["metadata"]["business"]
+            usecase=datarecord["metadata"]["usecase"]
+            ingestion_owner=datarecord["metadata"]["ingestion_owner"]
+            expiration=datarecord["metadata"]["expiration"]
+            level1_ownership=datarecord["metadata"]["level1_ownership"]
+            level2_ownership=datarecord["metadata"]["level2_ownership"]
+            confidentiality=datarecord["metadata"]["confidentiality"]
+            quality=datarecord["metadata"]["quality"]
+            refresh_velocity=datarecord["metadata"]["refresh_velocity"]
             
-            print("testing for source: "+ metadata_check["metadata"]["source"])
-            print("testing for prop: "+ str(metadata_check["prop"]))
-            print("testing for timestamp: "+ str(metadata_check["timestamp"]))
-            print("testing for retention: "+ metadata_check["metadata"]["retention"])
-            print("testing for business: "+ metadata_check["metadata"]["business"])
-            print("testing for data: "+ str(metadata_check["data"]))
-            print("testing for usecase: "+ metadata_check["metadata"]["usecase"])
-            print("testing for ingestion owner: "+ metadata_check["metadata"]["ingestion_owner"])
-            print("testing for level1_ownership: "+ metadata_check["metadata"]["level1_ownership"])
-            print("testing for level2_ownership: "+ metadata_check["metadata"]["level2_ownership"])
-            print("testing for confidentiality: "+ metadata_check["metadata"]["confidentiality"])
-            print("testing for expiration: "+ metadata_check["metadata"]["expiration"])
-            print("testing for URL: "+ metadata_check["metadata"]["url"])
-            print("testing for quality: "+ metadata_check["metadata"]["quality"])
-            print("testing for refresh_velocity: "+ metadata_check["metadata"]["refresh_velocity"])
-            
-            
-            
-            
-            data= BytesIO(metadata_check["data"].encode())
-            url= metadata_check["metadata"]["url"]
+            data= BytesIO(datarecord["data"].encode())
+            url= datarecord["metadata"]["url"]
             prefix=""
             url=url.split('/')
             n=len(url)
@@ -132,7 +93,7 @@ def lambda_handler(event, context):
             prefix=prefix+url[n-1]
             
           
-            s3.put_object(Bucket='onepurchase-test-s3-datalakebucket',Key=prefix, Body=data, ServerSideEncryption='AES256',Metadata={
+            s3.put_object(Bucket='<s3 target bucket>',Key=prefix, Body=data, ServerSideEncryption='AES256',Metadata={
         'retention': retention,
         'usecase': usecase,
         'quality': quality,
@@ -150,3 +111,4 @@ def lambda_handler(event, context):
 
             
     return 'Operation is successfully Completed'
+
